693fafa6e46b9aa89e325ec90d0bf773bf94f2c5
Nalexx6 <nikita2002oleksienko@gmail.com>
2023-10-25 22:51:26 +0300

    NFA to regex using Gauss method refactored to use left-linear equations

--- lab1/main.py
+++ lab1/main.py
@@ -8,59 +8,46 @@ def find_regex_from_nfa(nfa):
     # Initialize the matrix for storing equations
     matrix = [[None] * (num_states + 1) for _ in range(num_states)]
 
-    for i in range(1, num_states):
-        matrix[i][num_states] = 'e'
+    matrix[0][num_states] = 'e'
 
     # Create equations based on transitions
     for i in range(num_states):
         for j in range(num_states):
             if j in nfa[states[i]]:
-                print(f'{j} is direct from {states[i]}')
+                print(f'{states[j]} is direct from {states[i]}')
                 matrix[i][j] = nfa[states[i]][j]
             else:
-                print(f'{j} is not direct from {states[i]}')
+                print(f'{states[j]} is not direct from {states[i]}')
                 matrix[i][j] = None  # ∅ for no transition
 
-    print(matrix)
     # Apply Gauss method
 
     # Straight course
-    for k in range(num_states):
+    for k in range(num_states - 1, -1, -1):
         expr = f"({matrix[k][k]})*"
-        for i in range(k + 1, num_states):
-            matrix[k][k+1] += expr
-            matrix[k+1][i] += f"({matrix[k+1][i]})" + '|' + f"({matrix[k][k+1]}" + f"{matrix[i][k]})"
+        for i in range(k - 1, -1, -1):
+            if matrix[k][k-1] is not None and matrix[k-1][i] is not None and matrix[i][k] is not None:
+                matrix[k][k-1] += expr
+                matrix[k-1][i] += f"({matrix[k-1][i]})" + '|' + f"({matrix[k][k-1]}" + f"{matrix[i][k]})"
 
         matrix[k][k] = expr
 
-    print(matrix)
-
-    res = {states[num_states - 1]: matrix[num_states - 1][num_states - 1]}
+    res = {states[0]: matrix[0][0]}
 
     # Opposite course
-    for k in range(num_states - 2, -1, -1):
-        expr = ""
-        for i in range(k + 1, num_states):
-            expr += matrix[i][i] + matrix[k][i]
+    for k in range(1, num_states):
+        for i in range(0, k):
+            expr += "(" + matrix[i][i] + matrix[k][i] + ")"
+            if i < k - 1:
+                expr += '|'
 
-        if matrix[k][num_states] is not None:
-            expr += matrix[k][num_states]
-
-        matrix[k][k] = expr
-        res[states[k]] = expr
-
-    print(matrix)
+        matrix[k][k] = "(" + expr + ")" + matrix[k][k]
+        res[states[k]] = matrix[k][k]
 
     # Extract regular expressions for each state
     regex_dict = {}
     for i in range(num_states):
         regex_dict[states[i]] = matrix[i][i]
-
-    # Simplify the regular expressions
-    for i, r in regex_dict.items():
-        simplified_regex = re.sub(r'\(\(.*?\)\)\*', r'(\1)*', r)  # Remove redundant parentheses
-        simplified_regex = re.sub(r'\(\?P<[^>]+>', '(', simplified_regex)  # Replace named capture groups
-        regex_dict[i] = simplified_regex
 
     return res
 
@@ -69,9 +56,15 @@ if __name__ == '__main__':
 
     # Example usage:
     nfa_example = {
-        'q1': {0: '0', 1: '1'},
-        'q2': {0: '0', 1: '1'},
+        'q1': {0: '0', 1: '0'},
+        'q2': {0: '1', 1: '1'},
     }
+
+    # nfa_example = {
+    #     '0': {0: '[x]'},
+    #     '1': {0: '[x,y]', 1: '[y]'},
+    #     '2': {0: '[x,y,z]', 1: '[y]', 2: '[z]'},
+    # }
 
     result = find_regex_from_nfa(nfa_example)
 
